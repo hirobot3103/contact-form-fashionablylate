9d80a8ca2f7df94ff3fcde76c78e3606d0e42cf8
hirobot3103 <hirobot3103@gmail.com>
2024-12-21 20:19:57 +0900

    feature:入力確認画面送信前

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -21,17 +21,43 @@ class ContactRequest extends FormRequest
      */
     public function rules(): array
     {
+        // buildingはルールなし nullable:true
+        // detail size:120->max:120へ
         return [
             'last_name' => ['required', 'string', 'max:255'],
             'first_name' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'integer', 'between:1,3'],
             'email' => ['required', 'string', 'email' , 'max:255'],
-            'tel1' => ['required', 'digits_between:1,5'],
-            'tel2' => ['required', 'digits_between:1,5'],
-            'tel3' => ['required', 'digits_between:1,5'],
+            'tel1' => ['required', 'max:5'],
+            'tel2' => ['required', 'max:5'],
+            'tel3' => ['required', 'max:5'],
+            // 'tel3' => ['required', 'digits_between:1,5'],
             'address' => ['required', 'string', 'max:255'],
             'category' => ['required', 'integer'],
             'detail' => ['required', 'max:120']
+        ];
+    }
+
+    public function messages(): array
+    {
+        // エラーメッセージのカスタマイズ
+        // 必須項目、形式、文字制限等
+        return [
+            'last_name.required' => "姓を入力してください",
+            'first_name.required' => "名を入力してください",
+            'gender.required' => '性別を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel1.required' => '電話番号を入力してください',
+            'tel1.max' => '電話番号は5桁までの数字で入力してください',
+            'tel2.required' => '電話番号を入力してください',
+            'tel2.max' => '電話番号は5桁までの数字で入力してください',
+            'tel3.required' => '電話番号を入力してください',
+            'tel3.max' => '電話番号は5桁までの数字で入力してください',
+            'address.required' => '住所を入力してください',
+            'category.required' => 'お問い合わせの種類を入力してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
         ];
     }
 }
