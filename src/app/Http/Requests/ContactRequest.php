8ce9566415c08cc563cd50ebc3a614319650c1f1
hirobot3103 <hirobot3103@gmail.com>
2025-01-01 10:40:13 +0900

    mod fix

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -6,23 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
-        // buildingはルールなし nullable:true
-        // detail size:120->max:120へ
         return [
             'last_name' => ['required', 'string', 'max:255'],
             'first_name' => ['required', 'string', 'max:255'],
@@ -31,7 +21,6 @@ class ContactRequest extends FormRequest
             'tel1' => ['required', 'max:5'],
             'tel2' => ['required', 'max:5'],
             'tel3' => ['required', 'max:5'],
-            // 'tel3' => ['required', 'digits_between:1,5'],
             'address' => ['required', 'string', 'max:255'],
             'category' => ['required', 'integer'],
             'detail' => ['required', 'max:120']
